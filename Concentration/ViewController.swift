151f53a739ec8ee70ca2e0db1c5819dcfe50280e
me <lisamax@gmail.com>
2023-11-06 14:01:01 +0300

    full lab2 commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Maksymova Yelyzaveta on 25.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imageMain: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageMain.layer.cornerRadius = 50
+    }
+
+
+    
+
+
+}
